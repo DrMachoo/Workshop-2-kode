9a1c8668ca89649ed84948872d3c50e2d5795ff8
David Bach <60426203+DrMachoo@users.noreply.github.com>
2024-10-22 15:06:47 +0200

    Commit

---
+++ cmake-build-debug/CMakeFiles/SearchCompare.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SearchCompare.dir/main.c.o"
+  "CMakeFiles/SearchCompare.dir/main.c.o.d"
+  "SearchCompare"
+  "SearchCompare.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SearchCompare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
